8bb7c9909c8fa9b2d08969f772a1eb2090b764c5
NinePiece2 <romit.sagu@hotmail.com>
2024-10-06 18:36:29 -0400

    Add comments

--- Lab3/file_transfer_client.c
+++ Lab3/file_transfer_client.c
@@ -7,44 +7,45 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define SERVER_TCP_PORT 3000    /* Default port */
+#define SERVER_TCP_PORT 15000    /* Default server port */
 #define BUFLEN 256              /* Buffer length */
 
 int main(int argc, char *argv[]) {
-    int sd, port;
-    struct hostent *hp;
-    struct sockaddr_in server;
-    char buffer[BUFLEN], filename[BUFLEN];
-    int n;
+    int sd, port; // Socket Descriptor and port
+    struct hostent *hp; // Pointer to a hostent used for DNS lookup
+    struct sockaddr_in server; // Server address
+    char buffer[BUFLEN], filename[BUFLEN]; // Buffer's incoming data from server and filename that is taken from the user
+    int n; // Stores the number of bytes read
 
+    /* Checks if the number of arguments given is valid, port is optional defualts to 15000*/
     if (argc < 2 || argc > 3) {
         fprintf(stderr, "Usage: %s <server_host> [port]\n", argv[0]);
         exit(1);
     }
 
-    char *host = argv[1];
-    port = (argc == 3) ? atoi(argv[2]) : SERVER_TCP_PORT;
+    char *host = argv[1]; // gets the host ip as a string
+    port = (argc == 3) ? atoi(argv[2]) : SERVER_TCP_PORT; // if the port is given use it or else use the defualt port
 
     /* Create a TCP stream socket */
-    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) { // Creates a TCP Socket (SOCK_STREAM) using IPv4 (AF_INET)
         fprintf(stderr, "Can't create a socket\n");
         exit(1);
     }
 
     /* Set up the server address structure */
-    bzero((char *)&server, sizeof(struct sockaddr_in));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
+    bzero((char *)&server, sizeof(struct sockaddr_in)); // Creates a server address structure initialized to 0
+    server.sin_family = AF_INET; // IPv4
+    server.sin_port = htons(port); // Converts the port number to a network byte
 
     /* Get the host address */
-    if ((hp = gethostbyname(host)) == NULL) {
+    if ((hp = gethostbyname(host)) == NULL) { // Resolves the server's hostname to a IP Address
         fprintf(stderr, "Can't get server's address\n");
         exit(1);
     }
-    bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+    bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length); // Copies the server's IP Address to the server address structure
 
     /* Connect to the server */
-    if (connect(sd, (struct sockaddr *)&server, sizeof(server)) == -1) {
+    if (connect(sd, (struct sockaddr *)&server, sizeof(server)) == -1) { // Connects to the server using the socket descriptor and the server address structure
         fprintf(stderr, "Can't connect to server\n");
         exit(1);
     }
@@ -52,12 +53,12 @@ int main(int argc, char *argv[]) {
     /* Get the filename from the user */
     printf("Enter the filename to request from the server: ");
     fgets(filename, BUFLEN, stdin);
-    filename[strlen(filename) - 1] = '\0';  // Remove the newline character
+    filename[strlen(filename) - 1] = '\0';  // Remove the newline character from enter
 
-    /* Send the filename to the server */
+    /* Send the filename to the server over the socket using the descriptor */
     write(sd, filename, strlen(filename));
 
-    /* Receive the file or error message from the server */
+    /* Tries to create the file locally to store the recived data, on failure closes the socket */
     FILE *file = fopen(filename, "wb");
     if (file == NULL) {
         fprintf(stderr, "Error opening file to write received data.\n");
@@ -65,21 +66,23 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    /* Continously reads data from the socket/server */
     while ((n = read(sd, buffer, BUFLEN)) > 0) {
-        if (buffer[0] == 'E') {
+        if (buffer[0] == 'E') { // If the message starts with a E it is an error
             // Handle error message
             printf("Error from server: %s\n", buffer + 1);
 			remove(filename); // Delete the created file if there is an error from the server
             break;
-        } else if (buffer[0] == 'F') {
+        } else if (buffer[0] == 'F') { // If the message starts with F it contains file data and shoud be written to the file
             // Write file data to the file
-            fwrite(buffer + 1, sizeof(char), n - 1, file);
+            fwrite(buffer + 1, sizeof(char), n - 1, file); // Starting address / size of character / number of elements to write / file to write to
         }
     }
 
-    fclose(file);
+    fclose(file); // Closes the file and socket
     close(sd);
 
+    /* Prints the result of the file transfer */
     if (n == 0) {
         printf("File transfer complete.\n");
     } else if (n < 0) {
--- Lab3/file_transfer_server.c
+++ Lab3/file_transfer_server.c
@@ -6,9 +6,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define SERVER_TCP_PORT 3000    /* Default port */
+#define SERVER_TCP_PORT 15000    /* Default server port */
 #define BUFLEN 256              /* Buffer length */
 
+/* Process client requests */
 void handle_client(int new_sd) {
     char filename[BUFLEN];
     char buffer[BUFLEN];
@@ -22,68 +23,69 @@ void handle_client(int new_sd) {
     }
     filename[n] = '\0'; // Null-terminate the string
 
-    printf("Requested file: %s\n", filename);
+    printf("Requested file: %s\n", filename); // Prints the filename to the server's console
 
-    // Try to open the file
+    // Try to open the file, send an error if it can't
     FILE *file = fopen(filename, "rb");
     if (file == NULL) {
         // If the file can't be opened, send an error message
         buffer[0] = 'E'; // Error indicator
-        strcpy(buffer + 1, "Error: File not found or cannot be opened.\n");
+        strcpy(buffer + 1, "Error: File not found or cannot be opened.\n"); // Copies to the buffer's array of chars
         write(new_sd, buffer, strlen(buffer) + 1);
         close(new_sd);
         return;
     }
 
-    // Send the file contents to the client
+    // Send the file contents to the client in chunks 256 bytes a chunk of data
     buffer[0] = 'F'; // File indicator
     while ((n = fread(buffer + 1, sizeof(char), BUFLEN - 1, file)) > 0) {
         write(new_sd, buffer, n + 1); // Send the packet with data
     }
 
     fclose(file);
-    close(new_sd); // Close the connection after the transfer is complete
+    close(new_sd); // Close the connection to the client after the transfer is complete
 }
 
 int main(int argc, char *argv[]) {
-    int sd, new_sd, client_len, port;
-    struct sockaddr_in server, client;
+    int sd, new_sd, client_len, port; // Socket descriptor for the server / sd of the client / length of the client address struct / server listen port
+    struct sockaddr_in server, client; // Two structures for the server's address and connected client's information
 
-    port = (argc == 2) ? atoi(argv[1]) : SERVER_TCP_PORT;
+    port = (argc == 2) ? atoi(argv[1]) : SERVER_TCP_PORT; // If the server port is given by the user use it or else use the defualt
 
     /* Create a TCP stream socket */
-    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) { // Creates a TCP Socket (SOCK_STREAM) using IPv4 (AF_INET)
         fprintf(stderr, "Can't create a socket\n");
         exit(1);
     }
 
     /* Set up the server address structure */
-    bzero((char *)&server, sizeof(struct sockaddr_in));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    bzero((char *)&server, sizeof(struct sockaddr_in)); // Creates a server address structure initialized to 0
+    server.sin_family = AF_INET; // IPv4
+    server.sin_port = htons(port); // Converts the port number to a network byte
+    server.sin_addr.s_addr = htonl(INADDR_ANY); // Allows the server to accept connections on any of its IP addresses
 
     /* Bind the socket */
-    if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1) {
+    if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1) { // Tries to bind the socket to the server address 
         fprintf(stderr, "Can't bind name to socket\n");
         exit(1);
     }
 
     /* Listen for connections */
-    listen(sd, 5);
+    listen(sd, 5); // 5 is the max queue of pending connections
 
-    printf("Server listening on port %d\n", port);
+    printf("Server listening on port %d\n", port); // Logs to the server's console
 
+    /* Infinite loop to accept requests */
     while (1) {
-        client_len = sizeof(client);
-        new_sd = accept(sd, (struct sockaddr *)&client, &client_len);
+        client_len = sizeof(client); // Value used to indicate the amount of memory allocated for the client's address
+        new_sd = accept(sd, (struct sockaddr *)&client, &client_len); // Wait's for an incoming connection and accepts it. Set to the new socket descriptor
         if (new_sd < 0) {
             fprintf(stderr, "Can't accept client connection\n");
             exit(1);
         }
-        handle_client(new_sd);
+        handle_client(new_sd); // Calls the function with the socket which handles the file transfer
     }
 
-    close(sd);
+    close(sd); // Closes the socket and ends the program (doesn't hit because of infinate loop)
     return 0;
 }
