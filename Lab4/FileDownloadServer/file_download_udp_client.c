d1c644dd6154776cd441e1b459260731ad5dcc29
NinePiece2 <romit.sagu@hotmail.com>
2024-10-23 22:53:48 -0400

    Add comments

--- Lab4/FileDownloadServer/file_download_udp_client.c
+++ Lab4/FileDownloadServer/file_download_udp_client.c
@@ -11,19 +11,30 @@
 #define DEFAULT_PORT 15000    /* Default server port */
 #define BUFLEN 100            /* Buffer length for file data chunks */
 
+/*
+    Protocol Data Units (PDUs) Data Structure
+    Types:
+        - C -> Filename
+        - D -> Data
+        - F -> Final
+        - E -> Error
+
+    Data array for the PDU
+*/
 struct pdu {
     char type;
     char data[100];
 };
 
 int main(int argc, char *argv[]) {
-    int sd, port;
-    struct sockaddr_in server;
+    int sd, port; // socket descriptor and port
+    struct sockaddr_in server; // server address information
     struct hostent *hp;
     socklen_t server_len = sizeof(server);
-    struct pdu spdu;
-    FILE *file;
+    struct pdu spdu; // PDU instance to send and recieve data
+    FILE *file; // File object for sotring the downloaded file
     int n;
+    char filename[BUFLEN];
 
     // Check command-line arguments
     if (argc < 2 || argc > 3) {
@@ -31,20 +42,21 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    // Decodes the server IP and port
     char *server_ip = argv[1];
     port = (argc == 3) ? atoi(argv[2]) : DEFAULT_PORT;
 
     // Create UDP socket
-    if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+    if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) { // AF_INET -> IPv4 / SOCK_DGRAM -> UDP (User Datagram Protocal) / Protocal
         perror("Can't create socket");
         exit(1);
     }
 
     // Set up the server address structure
-    bzero((char *)&server, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    if (inet_pton(AF_INET, server_ip, &server.sin_addr) <= 0) {
+    bzero((char *)&server, sizeof(server)); // Creates a server address structure initialized to 0
+    server.sin_family = AF_INET; // IPv4
+    server.sin_port = htons(port);  // Converts the port number to a network byte
+    if (inet_pton(AF_INET, server_ip, &server.sin_addr) <= 0) { // Converts the given server IP Address from text to binary AF_INET -> IPv4 / IP in text / Binary destination
         fprintf(stderr, "Invalid server IP address\n");
         exit(1);
     }
@@ -54,12 +66,13 @@ int main(int argc, char *argv[]) {
     fgets(spdu.data, sizeof(spdu.data), stdin);
     spdu.data[strlen(spdu.data) - 1] = '\0';  // Remove newline
     spdu.type = 'C';
+    strncpy(filename, spdu.data, sizeof(filename) - 1);
 
     // Send filename PDU to the server
     sendto(sd, &spdu, sizeof(spdu), 0, (struct sockaddr *)&server, server_len);
 
     // Open local file to save data
-    file = fopen(spdu.data, "wb");
+    file = fopen(spdu.data, "wb"); // wb -> write binary
     if (file == NULL) {
         perror("Error opening file");
         close(sd);
@@ -67,15 +80,16 @@ int main(int argc, char *argv[]) {
     }
 
     // Receive file data
-    while ((n = recvfrom(sd, &spdu, sizeof(spdu), 0, (struct sockaddr *)&server, &server_len)) > 0) {
+    // Socket Descriptor / Buffer to store recived data / flags / source address object / source address object size
+    while ((n = recvfrom(sd, &spdu, sizeof(spdu), 0, (struct sockaddr *)&server, &server_len)) > 0) { 
         if (spdu.type == 'E') {
+            remove(filename); // Delete the newly created file if there is an error
             printf("Error: %s\n", spdu.data);
-            remove(spdu.data); // Delete file if error
             break;
         } else if (spdu.type == 'D') {
-            fwrite(spdu.data, sizeof(char), n - 1, file);
+            fwrite(spdu.data, sizeof(char), n - 1, file); // Writes the recieved data to the file
         } else if (spdu.type == 'F') {
-            printf("File transfer complete.\n");
+            printf("File transfer complete.\n"); // Stops the program once done
             break;
         }
     }
--- Lab4/FileDownloadServer/file_download_udp_server.c
+++ Lab4/FileDownloadServer/file_download_udp_server.c
@@ -10,19 +10,34 @@
 #define DEFAULT_PORT 15000    /* Default server port */
 #define BUFLEN 100            /* Buffer length for file data chunks */
 
+/*
+    Protocol Data Units (PDUs) Data Structure
+    Types:
+        - C -> Filename
+        - D -> Data
+        - F -> Final
+        - E -> Error
+
+    Data array for the PDU
+*/
 struct pdu {
     char type;
     char data[100];
 };
 
+/*
+    sd -> Socket Descriptor
+    client -> Client Address information
+    client_len -> The length of the Client Address information object
+*/
 void handle_client(int sd, struct sockaddr_in *client, socklen_t client_len) {
-    struct pdu spdu;
+    struct pdu spdu; // Variables needed for the file transfer
     char buffer[BUFLEN];
     int n;
     FILE *file;
 
-    // Receive the filename from the client
-    n = recvfrom(sd, &spdu, sizeof(spdu), 0, (struct sockaddr *)client, &client_len);
+    // Receive the filename (1st PDU) from the client
+    n = recvfrom(sd, &spdu, sizeof(spdu), 0, (struct sockaddr *)client, &client_len); // Socket Descriptor / Buffer to store recived data / flags / source address object / source address object size
     if (n < 0) {
         perror("Error receiving filename");
         return;
@@ -32,54 +47,55 @@ void handle_client(int sd, struct sockaddr_in *client, socklen_t client_len) {
     printf("Requested file: %s\n", spdu.data);
 
     // Open the file, send an error PDU if it can't be opened
-    file = fopen(spdu.data, "rb");
+    file = fopen(spdu.data, "rb"); // rb -> read bunary
     if (file == NULL) {
         spdu.type = 'E';
-        snprintf(spdu.data, sizeof(spdu.data), "Error: File not found or cannot be opened.");
-        sendto(sd, &spdu, sizeof(spdu), 0, (struct sockaddr *)client, client_len);
+        snprintf(spdu.data, sizeof(spdu.data), "File not found or cannot be opened."); // Set the spdu data to the error message
+        printf("Error: %s\n", spdu.data); // Log the error to the server console
+        sendto(sd, &spdu, sizeof(spdu), 0, (struct sockaddr *)client, client_len); // Send the pdu to the client and exit the process
         return;
     }
 
-    // Send the file contents to the client in chunks
+    // Send the file contents to the client in chunks with PDUs of type D
     spdu.type = 'D';
-    while ((n = fread(spdu.data, sizeof(char), sizeof(spdu.data), file)) > 0) {
+    while ((n = fread(spdu.data, sizeof(char), sizeof(spdu.data), file)) > 0) { // output data buffer / size of elements to be read (char)/ number of elements to read / file to read 
         sendto(sd, &spdu, n + 1, 0, (struct sockaddr *)client, client_len);
+        // Socket Descriptor / data to be sent / number of bytes to send / for specail flags (none slected) / client address information / size of the address information
     }
 
     // Send final PDU
     spdu.type = 'F';
     sendto(sd, &spdu, 1, 0, (struct sockaddr *)client, client_len);
-
     fclose(file);
 }
 
 int main(int argc, char *argv[]) {
     int sd, port;
-    struct sockaddr_in server, client;
-    socklen_t client_len = sizeof(client);
+    struct sockaddr_in server, client; // address information about the server and client
+    socklen_t client_len = sizeof(client); // Gets the size of the client address information to send to the handle_client method
 
     // Determine the port from command-line arguments or use default
     port = (argc == 2) ? atoi(argv[1]) : DEFAULT_PORT;
 
     // Create a UDP socket
-    if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+    if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) { // AF_INET -> IPv4 / SOCK_DGRAM -> UDP (User Datagram Protocal) / Protocal
         perror("Can't create socket");
         exit(1);
     }
 
     // Set up the server address structure
-    bzero((char *)&server, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    bzero((char *)&server, sizeof(server)); // Creates a server address structure initialized to 0
+    server.sin_family = AF_INET; // IPv4
+    server.sin_port = htons(port); // Converts the port number to a network byte
+    server.sin_addr.s_addr = htonl(INADDR_ANY); // Allows the server to accept connections on any of its IP addresses
 
     // Bind the socket to the address
-    if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1) {
-        perror("Can't bind socket");
+    if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1) { // socket descriptor / server address information / size of server address information
+        perror("Can't bind socket"); // on bind fail
         exit(1);
     }
 
-    printf("Server listening on port %d\n", port);
+    printf("Server listening on port %d\n", port); // Log port used for debugging
 
     // Main loop to handle client requests
     while (1) {
